87cd0da5afa3e96c67ce7bf10ea0f4153a11a561
Tom Ross <tom.ross@protonmail.com>
2021-06-17 00:20:42 +0800

    [UPD] Reduced CCC job script done

--- CHAP8/jobs.sh
+++ CHAP8/jobs.sh
@@ -7,66 +7,80 @@ echo "compiling reduced CCC code"
 
 # # kgrid
 
-# # kgrid set
-# k_set="1 2"
+# kgrid set
+k_set="1 2"
 
-# # theta set
-# th_set="0 1 2"
+# theta set
+th_set="0 1 2"
 
-# # kgrid jobs
-# for k in ${k_set} ; do
-#   echo "k: ${k}"
-#   for th in ${th_set} ; do
-#     echo "theta: ${th}"
+# kgrid jobs
+for k in ${k_set} ; do
+  for th in ${th_set} ; do
+    echo ""
+    echo "k: ${k}"
+    echo "theta: ${th}"
 
-#     dir="./data/k${k}_th${th}/"
+    dir="./data/kgrid/${k}_${th}/"
 
-#     echo "job: ${dir}"
+    # if dir already exists, job has already been run, hence skip
+    if [ -d ${dir} ] ; then
+      echo "already done"
+      continue
+    fi
 
-#     echo "staging ccc.in"
-#     cp -v "${dir}ccc.in" ./ccc.in
+    echo "making directory"
+    mkdir -v -p ${dir}
 
-#     echo "executing ./ccc"
-#     ./ccc < ccc.in > ccc.out
+    echo "creating ccc.in"
+    sed "2s|[^, ]*|${th}.0|6" "kgrid${k}.in" > "${dir}ccc.in"
 
-#     echo "extracting output"
-#     mv -v ccc.out ${dir}
-#     mv -v singlet* ${dir}
-#     mv -v triplet* ${dir}
-#     mv -v totalcs ${dir}
+    echo "staging ccc.in"
+    cp -v "${dir}ccc.in" ./ccc.in
 
-#     echo ""
-#   done
-# done
+    echo "executing ./ccc"
+    ./ccc < ccc.in > ccc.out
+
+    echo "extracting output"
+    mv -v ccc.out ${dir}
+    mv -v singlet* ${dir}
+    mv -v triplet* ${dir}
+    mv -v totalcs ${dir}
+  done
+done
 
 # book jobs
 
 # energy set
-en_set=$(echo {1..100..10})
+en_set=$(echo {1..100..1})
 
 echo ${en_set}
 for en in ${en_set} ; do
+  echo ""
   echo "energy: ${en}"
 
-  dir="./data/book/en_${en}/"
+  dir="./data/book/${en}/"
+
+  # if dir already exists, job has already been run, hence skip
+  if [ -d ${dir} ] ; then
+    echo "already done"
+    continue
+  fi
 
   echo "making directory"
   mkdir -v -p ${dir}
 
   echo "creating ccc.in"
-  sed "1 s/^[^,]*/${en}/g" book.in > "${dir}ccc.in"
+  sed "1s|[^, ]*|${en}.0|1" book.in > "${dir}ccc.in"
 
-  # echo "staging ccc.in"
-  # cp -v "${dir}ccc.in" ./ccc.in
+  echo "staging ccc.in"
+  cp -v "${dir}ccc.in" ./ccc.in
 
-  # echo "executing ./ccc"
-  # ./ccc < ccc.in > ccc.out
+  echo "executing ./ccc"
+  ./ccc < ccc.in > ccc.out
 
-  # echo "extracting output"
-  # mv -v ccc.out ${dir}
-  # mv -v singlet* ${dir}
-  # mv -v triplet* ${dir}
-  # mv -v totalcs ${dir}
-
-  echo ""
+  echo "extracting output"
+  mv -v ccc.out ${dir}
+  mv -v singlet* ${dir}
+  mv -v triplet* ${dir}
+  mv -v totalcs ${dir}
 done
